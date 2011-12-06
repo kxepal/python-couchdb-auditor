2ba3b442f6247a88846967a9c380d7aa5029c2b8
Alexander Shorin <Alexander Shorin@localhost>
2011-12-06 23:38:22 +0300

    Add check for CVE issues.

--- couchdb_auditor/auditor.py
+++ couchdb_auditor/auditor.py
@@ -8,10 +8,39 @@
 #
 
 import re
+import textwrap
 
 _RULES = {
     'server': []
 }
+
+_CVES = [
+    { 'id': 'CVE-2010-0009',
+      'url': 'http://web.nvd.nist.gov/view/vuln/detail?vulnId=CVE-2010-0009',
+      'overview': ('Apache CouchDB 0.8.0 through 0.10.1 allows remote attackers'
+                   ' to obtain sensitive information by measuring the'
+                   ' completion time of operations that verify (1) hashes or'
+                   ' (2) passwords.'),
+      'applies': lambda *version: (0, 8, 0) <= version <= (0, 10, 1)
+    },
+    { 'id': 'CVE-2010-2234',
+      'url': 'http://web.nvd.nist.gov/view/vuln/detail?vulnId=CVE-2010-2234',
+      'overview': ('Cross-site request forgery (CSRF) vulnerability in'
+                   ' Apache CouchDB 0.8.0 through 0.11.0 allows remote'
+                   ' attackers to hijack the authentication of administrators'
+                   ' for direct requests to an installation URL.'),
+      'applies': lambda *version: (0, 8, 0) <= version <= (0, 11, 0)
+    },
+    { 'id': 'CVE-2010-3854',
+      'url': 'http://web.nvd.nist.gov/view/vuln/detail?vulnId=CVE-2010-3854',
+      'overview': ('Multiple cross-site scripting (XSS) vulnerabilities in the'
+                   ' web administration interface (aka Futon) in'
+                   ' Apache CouchDB 0.8.0 through 1.0.1 allow remote attackers'
+                   ' to inject arbitrary web script or HTML via unspecified'
+                   ' vectors.'),
+      'applies': lambda *version: (0, 8, 0) <= version <= (1, 0, 1)
+    }
+]
 
 def server_rule(func):
     _RULES['server'].append(func)
@@ -42,3 +71,25 @@ def check_version(server, log):
             log.warn('You have using non release version: %s', version)
         else:
             log.info('CouchDB version: %s', version)
+
+@server_rule
+def check_CVE_issues(server, log):
+    version = server.version()
+
+    v_match = re.match('^(?P<major>\d+)\.(?P<minor>\d+)\.(?P<revision>\d+).*$',
+                       version)
+    if v_match is None:
+        log.warn('Unable to extract version info from: %s', version)
+        return
+
+    version = map(int, v_match.groups())
+    affected = False
+    for bill in _CVES:
+        if bill['applies'](*version):
+            overview = '\n'.join(textwrap.wrap(bill['overview'], 80))
+            log.error('CVE Vulnerability: %s\n%s\nSee %s',
+                      bill['id'], overview, bill['url'])
+            affected = True
+
+    if not affected:
+        log.info('Not affected by all known issues')
