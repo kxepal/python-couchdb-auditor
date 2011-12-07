48c5f707640fe0e216f1a454d6c52265d02de1e7
Alexander Shorin <Alexander Shorin@localhost>
2011-12-07 17:42:50 +0300

    Add setup.py script.

---
+++ setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2011 Alexander Shorin
+# All rights reserved.
+#
+# This software is licensed as described in the file COPYING, which
+# you should have received as part of this distribution.
+#
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name = 'couchdb-auditor',
+    version = '0.1',
+
+    description = \
+        'Audit configuration and security issues for an Apache CouchDB server.',
+    long_description = \
+        'Python port of Jason Smith https://github.com/iriscouch/audit_couchdb tool.',
+    author = 'Alexander Shorin',
+    author_email = 'kxepal@gmail.com',
+    license = 'BSD',
+    url = 'http://code.google.com/p/couchdb-auditor/',
+
+    install_requires = ['couchdb'],
+    entry_points = {
+        'console_scripts': [
+            'couchdb-auditor = couchdb_auditor:main',
+        ],
+    },
+
+    packages = ['couchdb_auditor'],
+)
